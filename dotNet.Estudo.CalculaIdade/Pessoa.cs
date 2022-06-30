d8c4b389edd7b9b1b1ab78c87e8be2f6196c930f
Marcelly Marsura <marcellymarsura@gmail.com>
2022-06-29 23:31:13 -0300

    Criando classe pessoa e cadastro

---
+++ dotNet.Estudo.CalculaIdade/Pessoa.cs
@@ -0,0 +1,65 @@
+﻿namespace dotNet.Estudo.CalculaIdade
+{
+    internal class Pessoa
+    {
+        static int id;
+        private string nome;
+        private DateTime dataNascimento;
+
+        public int Id { get; private set; }
+
+        public string Nome
+        {
+            get => nome;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Nome inválido");
+                nome = value;
+            }
+        }
+
+        public DateTime DataNascimento
+        {
+            get => dataNascimento;
+            set
+            {
+                if (value >= DateTime.Now)
+                    throw new Exception("Data inválida");
+                dataNascimento = value;
+            }
+        }
+
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, DateTime dataNascimento)
+        {
+            id++;
+            Id = id;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Idade = CalculaIdade();
+        }
+
+        private int CalculaIdade()
+        {
+            DateTime dataAtual = DateTime.Now;
+
+            if(dataAtual.Month < DataNascimento.Month || (dataAtual.Month == DataNascimento.Month && dataAtual.Day < DataNascimento.Day))
+            {
+                return dataAtual.Year - DataNascimento.Year - 1;
+            }
+
+            return dataAtual.Year - DataNascimento.Year;
+        }
+
+        public override string ToString()
+        {
+            string texto = $"Id: {Id}" + Environment.NewLine
+                         + $"Nome: {Nome}" + Environment.NewLine
+                         + $"Data de Nascimento: {DataNascimento.ToString("dd/MM/yyyy")}" + Environment.NewLine
+                         + $"Idade: {Idade} anos" + Environment.NewLine;
+            return texto;
+        }
+    }
+}
---
+++ dotNet.Estudo.CalculaIdade/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace dotNet.Estudo.CalculaIdade
+{
+    public class Program
+    {
+        static List<Pessoa> pessoas = new List<Pessoa>();
+        
+        public static void Main(String[] args)
+        {
+            do
+            {
+                NovoCadastro();
+            } while (Console.ReadKey().Key != ConsoleKey.S);
+
+            ExibirCadastros();  
+        }
+
+        static void NovoCadastro()
+        {
+            string nome;
+            DateTime dataNascimento;
+            Pessoa p;
+            Console.Clear();
+            Console.WriteLine("NOVO CADASTRO:");
+            do
+            {
+                try
+                {
+                    Console.Write("\nNome: ");
+                    nome = Console.ReadLine().Trim();
+
+                    Console.Write("Data de Nascimento: ");
+                    dataNascimento = Convert.ToDateTime(Console.ReadLine());
+
+                    p = new Pessoa(nome: nome,
+                                   dataNascimento: dataNascimento);
+
+                    pessoas.Add(p);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"ERRO: {e.Message}");
+                }
+            } while (true);
+
+
+            Console.WriteLine("\nAperte S para sair ou qualquer tecla para continuar...");
+        }
+
+        static void ExibirCadastros()
+        {
+            Console.Clear();
+            Console.WriteLine("PESSOAS CADASTRADAS:\n");
+
+            foreach (var item in pessoas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
